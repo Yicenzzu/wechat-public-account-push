be36a75a820098649a494d244e43c070142770db
轶涔 <55955101+Yicenzzu@users.noreply.github.com>
2023-07-22 21:23:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'owYNv5yJb4ldC9YrDCfRMWZyaFwA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'wqw_jgseTJNcuR5f3RtB_dEKZm2r8TNR7LazHR6kveQ',
+      useTemplateId: 'nalmCTZ9XyTW7xGXyjNLmPxo70DxjbkQsZmCEtATjZI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
@@ -43,7 +43,7 @@ const USER_CONFIG = {
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-06-04' },
-        // 结婚纪念日
+        // kaoyan日
         { keyword: 'marry_day', date: '2023-12-20' },
       ],
     },
